c6098975d0ab788459beb409d1480ea52c4d88ff
OLIVER\subsh <subshadow11@gmail.com>
2018-05-02 22:46:18 -0500

    Funcionamiento del botón personalizado

--- app/src/main/java/e/subsh/proyectotsp/MenuPrincipal.java
+++ app/src/main/java/e/subsh/proyectotsp/MenuPrincipal.java
@@ -1,13 +1,29 @@
 package e.subsh.proyectotsp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuPrincipal extends AppCompatActivity {
+
+    Button btnPersonalizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        btnPersonalizar = (Button) findViewById(R.id.btnPersonalizado);
+        btnPersonalizar.setOnClickListener(onClickPersonalizar);
     }
+
+    public View.OnClickListener onClickPersonalizar= new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intentdatos = new Intent(getApplicationContext(),Personalizado.class);
+            startActivity(intentdatos);
+        }
+    };
 }
