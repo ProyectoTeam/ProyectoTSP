f4b3ff1d14d152d3c19298a01bb08e83be1a9f1e
OLIVER\subsh <subshadow11@gmail.com>
2018-05-06 01:14:16 -0500

    Agregada las imágenes de la vista de personalizado. Para hacer las demás vistas necesito saber manejar archivos y pues aún no lo enseñan en clase.

--- app/src/main/java/e/subsh/proyectotsp/Personalizado.java
+++ app/src/main/java/e/subsh/proyectotsp/Personalizado.java
@@ -2,12 +2,22 @@ package e.subsh.proyectotsp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Personalizado extends AppCompatActivity {
+
+    String cuestionarios[];
+    Spinner spCuestionariosAbrir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personalizado);
+        spCuestionariosAbrir = (Spinner) findViewById(R.id.spAbrir);
+        cuestionarios = getResources().getStringArray(R.array.array_Cuestionarios);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,cuestionarios);
+        spCuestionariosAbrir.setAdapter(adapter);
     }
 }
