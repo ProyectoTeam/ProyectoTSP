58f7bea05cc654cb4ff381c405a61fa07928f436
alanarja410 <38296324+alanarja410@users.noreply.github.com>
2018-05-06 21:58:57 -0500

    ejemplo tipo 1

---
+++ app/src/main/java/e/subsh/proyectotsp/ejemplo_tipo_1.java
@@ -0,0 +1,13 @@
+package e.subsh.proyectotsp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ejemplo_tipo_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejemplo_tipo_1);
+    }
+}
