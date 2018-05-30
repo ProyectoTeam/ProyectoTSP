56bb8fd9fc3db9c17020f0a19f808bcda12c5506
JuanDiazPacheco <38867200+JuanDiazPacheco@users.noreply.github.com>
2018-05-29 21:53:17 -0500

    ya quedo la base x3

--- app/src/main/java/e/subsh/proyectotsp/Ejercicios.java
+++ app/src/main/java/e/subsh/proyectotsp/Ejercicios.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class Ejercicios extends AppCompatActivity {
 
@@ -11,10 +12,21 @@ public class Ejercicios extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicios);
+
+        BaseDatos_Cuestionario db=new BaseDatos_Cuestionario(getApplicationContext());
+
+        String[][] p=db.leer();
+
+
+
+      //  Toast.makeText(getApplicationContext(),p[0].toString(),Toast.LENGTH_LONG).show();
+
+
     }
 
     public void onClickMostrar(View view) {
         Intent intentamostrar=new Intent(getApplicationContext(),Ejercicio_Tipo_1.class);
+        intentamostrar.putExtra("id",1);//1 es al que de click
         startActivity(intentamostrar);
     }
 
